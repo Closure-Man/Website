7d8c77c4635eaf2987678dfcc4818c856729ec94
Ross Kilpatrick <rpkilpatrick@gmail.com>
2017-06-24 10:29:04 -0700

    Added the ability to send emails to captains through the contact page

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 //Centeral Node File
 
 //Requires
-let express = require('express'); //Express lets us do routing more easily
+const express = require('express'); //Express lets us do routing more easily
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
-let app = express();
+const app = express();
+const urlendcodedParser = bodyParser.urlencoded({extended: false});
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'redrockfrc@gmail.com',
+        pass: '#0Mn!wh3EL5'
+    }
+});
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -16,6 +28,29 @@ app.get('/', function(req, res)
 app.get('/contact', function(req, res)
 {
     res.render('contact');
+});
+
+app.post('/contact', urlendcodedParser, function(req, res)
+{
+    //if(!req.body) return res.send(400);
+    let mailOptions = {
+        from: req.body.senderEmail,
+        to: req.body.toEmail,
+        subject: req.body.subject,
+        text: req.body.senderEmail + ' ' + req.body.message
+    };
+    transporter.sendMail(mailOptions, function(error, info)
+    {
+        if(error)
+        {
+            console.log(error);
+            res.render('error');
+        }
+        else
+        {
+            res.render('success');
+        }
+    });
 });
 
 app.get('/about', function(req, res)
